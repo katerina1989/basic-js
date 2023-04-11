f1702b02957d4d77793e07d338d9f5d9cec5b1a6
Katerina Shibalovich <katerinaharak77775555@gmail.com>
2023-04-11 16:33:42 +0300

    refactor: добавлена постоянная полураспада

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,8 +4,8 @@ const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
 /**
- * Determine the age of archeological find by using
- * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
+ * Determine the age of archeological find by using ОПРЕДЕЛИТЬ ВОЗРАСТ АРХ.НАХОДКИ С ПОМОЩЬЮ
+ * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values     СОВРЕМЕННАЯ АКТИВНОСТЬ  ПОЛУПЕРИОД ЖИЗНИ
  * 
  * @param {String} sampleActivity string representation of current activity 
  * @return {Number | Boolean} calculated age in years or false
@@ -21,6 +21,7 @@ function dateSample(/* sampleActivity */) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
+/* l-постоянная полураспада карбонада=0.693 */
 
 module.exports = {
   dateSample
