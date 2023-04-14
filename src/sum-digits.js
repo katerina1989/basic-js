b6b72ccbe43c0b76d0791110ae43e6c11eba40dd
Katerina Shibalovich <katerinaharak77775555@gmail.com>
2023-04-14 11:02:51 +0300

    refactor:add task2

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,7 +12,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(n) {
+function getSumOfDigits(n, count = 0) {
   if (n < 10) return count;
   
   return getSumOfDigits(
